4fc1efbdb0cd0e0f373aad3dddfa6da6f06e38b3
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-23 05:54:04 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -20,7 +20,7 @@ def assembled_matrix [
     ["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"], 
     ["Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza", "Michael Pemulis"]
     ]
-    return assembled_matrix
+    puts assembled_matrix
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
 end
