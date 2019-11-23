192adc1bacfa2d6240ff5f24182bdcc81d29c5e8
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-23 06:00:08 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -24,6 +24,7 @@ def assembled_matrix
 end
 
 def array_literal_matrix
+  
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
